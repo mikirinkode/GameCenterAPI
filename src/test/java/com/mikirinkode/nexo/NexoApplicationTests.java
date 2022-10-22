ce66d73cdfa94752a07c846a244f9381d256af89
Muhammad Wafa <muhammadwafa105@gmail.com>
2022-10-22 22:29:42 +0700

    initial commit

---
+++ src/test/java/com/mikirinkode/nexo/NexoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikirinkode.nexo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
